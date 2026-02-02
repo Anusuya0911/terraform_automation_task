f72a726f72c6e8ff53769d083fca76126dfe0876
Anusuya0911 <anusuyadevi0911@gmail.com>
2026-02-02 14:03:49 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,22 @@
+resource "aws_instance" "example" {
+  ami           = "ami-00c9e0352375bce85"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+resource "aws_security_group" "new" {
+  ingress {
+    protocol  = "tcp"
+    from_port = 22
+    to_port   = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
