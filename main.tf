22c0d38d39d999216391cdf843b96cb4b2c831fa
Anusuya0911 <162282599+Anusuya0911@users.noreply.github.com>
2026-02-02 15:25:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-resource "aws_instance" "example" {
-  ami           = "ami-07edab31d909a5063"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "HelloWorld"
-  }
-}
 resource "aws_security_group" "new" {
   ingress {
     protocol  = "tcp"
@@ -18,5 +11,13 @@ resource "aws_security_group" "new" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_instance" "example" {
+  ami           = "ami-07edab31d909a5063"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.new.id]
+  tags = {
+    Name = "HelloWorld"
   }
 }
